5aa04714a010108f9f1110c974bf7cbe3a44e0cd
Santhosh Reddy Vootukuri (SUNNY) <nagavo@microsoft.com>
2024-09-26 05:58:00 -0700

    Adding Blazor Front end UI

---
+++ BenefitsManager.Frontend.WebPortal.Ui/Program.cs
@@ -0,0 +1,37 @@
+
+using BenefitsManager.Frontend.WebPortal.Ui.Services;
+using BenefitsManager.Frontend.WebPortal.Ui.Components;
+using BenefitsManager.Frontend.WebPortal.Ui.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5103") });
+builder.Services.AddScoped<ClaimService>();
+
+builder.Logging.SetMinimumLevel(LogLevel.Debug);
+builder.Logging.AddConsole();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
