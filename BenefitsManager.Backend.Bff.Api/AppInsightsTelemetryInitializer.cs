e6c23021312773f7f3dd506fd5348fcf6f2ce9da
Santhosh Reddy Vootukuri (SUNNY) <nagavo@microsoft.com>
2025-02-02 19:00:33 -0800

    Adding application insights telemetry related changes

---
+++ BenefitsManager.Backend.Bff.Api/AppInsightsTelemetryInitializer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace BenefitsManager.Backend.Bff.Api
+{
+    public class AppInsightsTelemetryInitializer : ITelemetryInitializer
+    {
+        public void Initialize(ITelemetry telemetry)
+        {
+            if (string.IsNullOrEmpty(telemetry.Context.Cloud.RoleName))
+            {
+                //set custom role name here
+                telemetry.Context.Cloud.RoleName = "benefitsmanager-backend-api";
+            }
+        }
+    }
+}
